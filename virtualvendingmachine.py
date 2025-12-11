50148686d683cc90ba1e5b5cf50ed8207e7eff7d
snowyyfare8 <snowzizst08@proton.me>
2025-12-11 16:47:15 +0300

    Added GUI, and beginning work on selector system
    Added GUI for the product display, dispenser, and a receipt, while also creating the selection system

--- virtualvendingmachine.py
+++ virtualvendingmachine.py
@@ -1,89 +1,238 @@
 # Virtual Vending Machine 
 import tkinter as tk
 import customtkinter as ctk
+from collections import Counter
 from vendingmachineproducts import vending_machine_products
 
 class master_frame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-
 class vending_display(ctk.CTkFrame):
     def __init__(self, master_frame, **kwargs):
         super().__init__(master_frame, **kwargs)
+        product_name_1 = "Meed Cookies"
+        product_id_price_1 = "100 - 2.00"
+        
+        product_name_2 = "Lays MAX Spicy"
+        product_id_price_2 = "101 - 4.00"
 
+        product_name_3 = "Lays Cheese-flavored Potato Chips"
+        product_id_price_3 = "102 - 4.00"
+
+        product_name_4 = "Doritos Nacho Cheese"
+        product_id_price_4 = "103 - 4.00"
+
+        product_name_5 = "Doritos Sweet Pepper"
+        product_id_price_5 = "104 - 4.00"
+
+        product_name_6 = "Almarai Chocolate Milk"
+        product_id_price_6 = "105 - 2.00"
+
+        product_name_7 = "Almarai Strawberry Milk"
+        product_id_price_7 = "106 - 2.00"
+        
+        product_name_8 = "M & M's"
+        product_id_price_8 = "107 - 4.00"
+
+        product_name_9 = "Al Rabia OJ"
+        product_id_price_9 = "108 - 3.00"
+
+        product_name_10 = "Mars Chocolate Bar"
+        product_id_price_10 = "109 - 4.00"
+
+        product_name_11 = "Galaxy Chocolate Bar"
+        product_id_price_11 = "110 - 4.00"
+
+        product_name_12 = "Canned Ice Tea"
+        product_id_price_12 = "111 - 2.00"
+
+        product_name_13 = "Aquafina Bottle Water"
+        product_id_price_13 = "112 - 4.00"
+
+        product_name_14 = "Packaged TLT Sandwich"
+        product_id_price_14 = "113 - 4.00"
+
+        product_name_15 = "Fruit Cake Slice"
+        product_id_price_15 = "114 - 2.00"
+
+        self.display_name_1 = ctk.CTkLabel(self, text = product_name_1)
+        self.display_idprice_1 = ctk.CTkLabel(self, text = product_id_price_1)
+
+        self.display_name_2 = ctk.CTkLabel(self, text = product_name_2)
+        self.display_idprice_2 = ctk.CTkLabel(self, text = product_id_price_2)
+
+        self.display_name_3 = ctk.CTkLabel(self, text = product_name_3)
+        self.display_idprice_3 = ctk.CTkLabel(self, text = product_id_price_3)
+
+        self.display_name_4 = ctk.CTkLabel(self, text = product_name_4)
+        self.display_idprice_4 = ctk.CTkLabel(self, text = product_id_price_4)
+
+        self.display_name_5 = ctk.CTkLabel(self, text = product_name_5)
+        self.display_idprice_5 = ctk.CTkLabel(self, text = product_id_price_5)
+
+        self.display_name_6 = ctk.CTkLabel(self, text = product_name_6)
+        self.display_idprice_6 = ctk.CTkLabel(self, text = product_id_price_6)
+
+        self.display_name_7 = ctk.CTkLabel(self, text = product_name_7)
+        self.display_idprice_7 = ctk.CTkLabel(self, text = product_id_price_7)
+
+        self.display_name_8 = ctk.CTkLabel(self, text = product_name_8)
+        self.display_idprice_8 = ctk.CTkLabel(self, text = product_id_price_8)
+
+        self.display_name_9 = ctk.CTkLabel(self, text = product_name_9)
+        self.display_idprice_9 = ctk.CTkLabel(self, text = product_id_price_9)
+
+        self.display_name_10 = ctk.CTkLabel(self, text = product_name_10)
+        self.display_idprice_10 = ctk.CTkLabel(self, text = product_id_price_10)
+
+        self.display_name_11 = ctk.CTkLabel(self, text = product_name_11)
+        self.display_idprice_11 = ctk.CTkLabel(self, text = product_id_price_11)
+
+        self.display_name_12 = ctk.CTkLabel(self, text = product_name_12)
+        self.display_idprice_12 = ctk.CTkLabel(self, text = product_id_price_12)
+
+        self.display_name_13 = ctk.CTkLabel(self, text = product_name_13)
+        self.display_idprice_13 = ctk.CTkLabel(self, text = product_id_price_13)
+
+        self.display_name_14 = ctk.CTkLabel(self, text = product_name_14)
+        self.display_idprice_14 = ctk.CTkLabel(self, text = product_id_price_14)
+
+        self.display_name_15 = ctk.CTkLabel(self, text = product_name_15)
+        self.display_idprice_15 = ctk.CTkLabel(self, text = product_id_price_15)
+
+        self.display_name_1.grid(row = 0, column = 0, padx = 5, pady = 2)
+        self.display_idprice_1.grid(row = 1, column = 0, padx = 5, pady = 2)
+
+        self.display_name_2.grid(row = 0, column = 1, padx = 5, pady = 2)
+        self.display_idprice_2.grid(row = 1, column = 1, padx = 5, pady = 2)
+
+        self.display_name_3.grid(row = 0, column = 2, padx = 5, pady = 2)
+        self.display_idprice_3.grid(row = 1, column = 2, padx = 5, pady = 2)
+
+        self.display_name_4.grid(row = 0, column = 3, padx = 5, pady = 2)
+        self.display_idprice_4.grid(row = 1, column = 3, padx = 5, pady = 2)
+
+        self.display_name_5.grid(row = 0, column = 4, padx = 5, pady = 2)
+        self.display_idprice_5.grid(row = 1, column = 4, padx = 5, pady = 2)
+
+        self.display_name_6.grid(row = 2, column = 0, padx = 5, pady = 2)
+        self.display_idprice_6.grid(row = 3, column = 0, padx = 5, pady = 2)
+
+        self.display_name_7.grid(row = 2, column = 1, padx = 5, pady = 2)
+        self.display_idprice_7.grid(row = 3, column = 1, padx = 5, pady = 2)
+
+        self.display_name_8.grid(row = 2, column = 2, padx = 5, pady = 2)
+        self.display_idprice_8.grid(row = 3, column = 2, padx = 5, pady = 2)
+
+        self.display_name_9.grid(row = 2, column = 3, padx = 5, pady = 2)
+        self.display_idprice_9.grid(row = 3, column = 3, padx = 5, pady = 2)
+
+        self.display_name_10.grid(row = 2, column = 4, padx = 5, pady = 2)
+        self.display_idprice_10.grid(row = 3, column = 4, padx = 5, pady = 2)
+
+        self.display_name_11.grid(row = 4, column = 0, padx = 5, pady = 2)
+        self.display_idprice_11.grid(row = 5, column = 0, padx = 5, pady = 2)
+
+        self.display_name_12.grid(row = 4, column = 1, padx = 5, pady = 2)
+        self.display_idprice_12.grid(row = 5, column = 1, padx = 5, pady = 2)
+
+        self.display_name_13.grid(row = 4, column = 2, padx = 5, pady = 2)
+        self.display_idprice_13.grid(row = 5, column = 2, padx = 5, pady = 2)
+
+        self.display_name_14.grid(row = 4, column = 3, padx = 5, pady = 2)
+        self.display_idprice_14.grid(row = 5, column = 3, padx = 5, pady = 2)
+
+        self.display_name_15.grid(row = 4, column = 4, padx = 5, pady = 2)
+        self.display_idprice_15.grid(row = 5, column = 4, padx = 5, pady = 2)
 
 class vending_selector(ctk.CTkFrame):
     def __init__(self, master_frame, **kwargs):
         super().__init__(master_frame, **kwargs)
         
-        def find_product_id(product_id):
+        def select_product_id(product_id):
+            
+            product = id_var.get()
+            id_var.set("")
+            print(id)
             for product in vending_machine_products:
                 if product["id"] == product_id:
-                    return product["name"], product["price"]
-                return ["Product Not Found"]
 
-        def update_entry(value):
+                    return product["name"] and product["price"]
+                return ["Product not found"]
             
+        id_var = tk.StringVar()
+        self.entry = ctk.CTkEntry(self, textvariable = id_var)
 
-
-        self.entry = ctk.CTkEntry(self, textvariable = entry, placeholder_text = "Enter product id...", state = "disabled")
-
-        self.button1 = ctk.CTkButton(self, text = "1", command = lambda:update_entry(1))
-        self.button2 = ctk.CTkButton(self, text = "2", command = lambda:update_entry(2))
-        self.button3 = ctk.CTkButton(self, text = "3", command = lambda:update_entry(3))
+        self.button1 = ctk.CTkButton(self, text = "1", command = lambda:self.entry.insert('end', 1))
+        self.button2 = ctk.CTkButton(self, text = "2", command = lambda:self.entry.insert('end', 2))
+        self.button3 = ctk.CTkButton(self, text = "3", command = lambda:self.entry.insert('end', 3))
     
-        self.button4 = ctk.CTkButton(self, text = "4", command = lambda:update_entry(4))
-        self.button5 = ctk.CTkButton(self, text = "5", command = lambda:update_entry(5))
-        self.button6 = ctk.CTkButton(self, text = "6", command = lambda:update_entry(6))
+        self.button4 = ctk.CTkButton(self, text = "4", command = lambda:self.entry.insert('end', 4))
+        self.button5 = ctk.CTkButton(self, text = "5", command = lambda:self.entry.insert('end', 5))
+        self.button6 = ctk.CTkButton(self, text = "6", command = lambda:self.entry.insert('end', 6))
 
-        self.button7 = ctk.CTkButton(self, text = "7", command = lambda:update_entry(7))
-        self.button8 = ctk.CTkButton(self, text = "8", command = lambda:update_entry(8))
-        self.button9 = ctk.CTkButton(self, text = "9", command = lambda:update_entry(9))
+        self.button7 = ctk.CTkButton(self, text = "7", command = lambda:self.entry.insert('end', 7))
+        self.button8 = ctk.CTkButton(self, text = "8", command = lambda:self.entry.insert('end', 8))
+        self.button9 = ctk.CTkButton(self, text = "9", command = lambda:self.entry.insert('end', 9))
 
-        self.button0 = ctk.CTkButton(self, text = "0", command = lambda:update_entry(0))
-        self.button_ok = ctk.CTkButton(self, text = "Ok", command = lambda:find_product_id)
-        self.button_cancel = ctk.CTkButton(self, text = "X",)
+        self.button0 = ctk.CTkButton(self, text = "0", command = lambda:self.entry.insert('end', 0))
+        self.button_ok = ctk.CTkButton(self, text = "Ok", command = select_product_id(product_id))
+        self.button_cancel = ctk.CTkButton(self, text = "X", command = lambda:self.entry.delete('end'))
 
-        self.button1.configure(height = 25, width = 140)
-        self.button2.configure(height = 25, width = 140)
-        self.button3.configure(height = 25, width = 140)
+        self.button1.configure(height = 25, width = 70)
+        self.button2.configure(height = 25, width = 70)
+        self.button3.configure(height = 25, width = 70)
 
-        self.button4.configure(height = 25, width = 140)
-        self.button5.configure(height = 25, width = 140)
-        self.button6.configure(height = 25, width = 140)
+        self.button4.configure(height = 25, width = 70)
+        self.button5.configure(height = 25, width = 70)
+        self.button6.configure(height = 25, width = 70)
 
-        self.button7.configure(height = 25, width = 140)
-        self.button8.configure(height = 25, width = 140)
-        self.button9.configure(height = 25, width = 140)
+        self.button7.configure(height = 25, width = 70)
+        self.button8.configure(height = 25, width = 70)
+        self.button9.configure(height = 25, width = 70)
 
-        self.button0.configure(height = 25, width = 140)
-        self.button_ok.configure(height = 25, width = 140)
-        self.button_cancel.configure(height = 25, width = 140) 
+        self.button0.configure(height = 25, width = 70)
+        self.button_ok.configure(height = 25, width = 70)
+        self.button_cancel.configure(height = 25, width = 70) 
 
         self.entry.grid(row = 4, column = 1, padx = 2, pady = 2)
 
-        self.button1.grid(row = 0, column = 0, padx = 2, pady = 2)
-        self.button2.grid(row = 0, column = 1, padx = 2, pady = 2)
-        self.button3.grid(row = 0, column = 2, padx = 2, pady = 2)
+        self.button1.grid(row = 0, column = 0, padx = 2, pady = 5)
+        self.button2.grid(row = 0, column = 1, padx = 2, pady = 5)
+        self.button3.grid(row = 0, column = 2, padx = 2, pady = 5)
 
-        self.button4.grid(row = 1, column = 0, padx = 2, pady = 2)
-        self.button5.grid(row = 1, column = 1, padx = 2, pady = 2)
-        self.button6.grid(row = 1, column = 2, padx = 2, pady = 2)
+        self.button4.grid(row = 1, column = 0, padx = 2, pady = 5)
+        self.button5.grid(row = 1, column = 1, padx = 2, pady = 5)
+        self.button6.grid(row = 1, column = 2, padx = 2, pady = 5)
 
-        self.button7.grid(row = 2, column = 0, padx = 2, pady = 2)
-        self.button8.grid(row = 2, column = 1, padx = 2, pady = 2)
-        self.button9.grid(row = 2, column = 2, padx = 2, pady = 2)
+        self.button7.grid(row = 2, column = 0, padx = 2, pady = 5)
+        self.button8.grid(row = 2, column = 1, padx = 2, pady = 5)
+        self.button9.grid(row = 2, column = 2, padx = 2, pady = 5)
 
-        self.button0.grid(row = 3, column = 1, padx = 2, pady = 2)
-        self.button_ok.grid(row = 3, column = 0, padx = 2, pady = 2)
-        self.button_cancel.grid(row = 3, column = 2, padx = 2, pady = 2) 
+        self.button0.grid(row = 3, column = 1, padx = 2, pady = 5)
+        self.button_ok.grid(row = 3, column = 0, padx = 2, pady = 5)
+        self.button_cancel.grid(row = 3, column = 2, padx = 2, pady = 5) 
 
+class vending_receipt(ctk.CTkFrame):
+    def __init__(self, master_frame, **kwargs):
+        super().__init__(master_frame, **kwargs)
+
+        self.cart = Counter()
+        vending_wallet = 150
+        self.wallet = ctk.CTkLabel(self, text = vending_wallet)
+        self.buy = ctk.CTkButton(self, text = "Purchase")
+
+        self.buy.grid(row = 0, column = 0, padx = 75, pady = 75, sticky = "nsew")
 
 class vending_dispenser(ctk.CTkFrame):
     def __init__(self, master_frame, **kwargs):
         super().__init__(master_frame, **kwargs)
-    
+
+        dispenser_state = tk.StringVar()
+        self.dispenser = ctk.CTkButton(self, textvariable = dispenser_state, text = "no product bought yet" )
+
+        self.dispenser.grid(row = 0, column = 0, padx = 285, pady = 75, sticky = "nsew")
 
 class App(ctk.CTk):
     def __init__(self):    
@@ -91,17 +240,20 @@ class App(ctk.CTk):
     
         self.title("Vending Machine Simulator")
 
-        self.Master_Frame = master_frame(master = self, height = 1200, width = 1200, corner_radius = 0, fg_color = "transparent")
+        self.Master_Frame = master_frame(master = self, height = 2700, width = 2700, corner_radius = 0, fg_color = "transparent")
         self.Master_Frame.grid(row = 0, column = 0, sticky = "nsew")
 
         self.display = vending_display(self.Master_Frame)
-        self.display.grid(row = 0, column = 0)
+        self.display.grid(row = 0, column = 0, sticky = "nw")
 
         self.selector = vending_selector(self.Master_Frame)
-        self.selector.grid(row = 0, column = 1)
+        self.selector.grid(row = 0, column = 1, sticky = "nse")
+
+        self.receipt = vending_receipt(self.Master_Frame)
+        self.receipt.grid(row = 1, column = 1, sticky = "nsew")
 
         self.dispenser = vending_dispenser(self.Master_Frame)
-        self.dispenser.grid(row = 3, column = 0)
+        self.dispenser.grid(row = 1, column = 0, sticky = "sew")
 
 app = App()
 app.mainloop()
